8c0404759bb633a73fdef760726f8591efc38ceb
Ivan Trifonov <thereason12d@gmail.com>
2023-10-17 12:33:55 +0300

    refactored code

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
 // Start, Pause and Reset game
-
-let timerInterval;
-let gamePaused = true;
-let seconds = 0;
-let minutes = 0;
-let lastClickTime = 0;
 const points = document.querySelector(".points-num");
 const buttons = document.querySelectorAll("button");
 const holes = document.querySelectorAll(".hole");
-let lastHole, newHole;
-let activeHole;
 const mole = document.createElement("img");
 mole.setAttribute("src", "mole.png");
+
+let timerInterval;
+let seconds = 0;
+let minutes = 0;
+let lastClickTime = 0;
+let lastHole, newHole;
+let activeHole;
 
 document.addEventListener("DOMContentLoaded", function () {
   mole.classList.add("mole");
   mole.classList.add("active-mole");
   mole.draggable = false;
   mole.addEventListener("click", () => {
-    if (!gamePaused) {
-      const now = Date.now(); // Get the current timestamp
-      if (now - lastClickTime >= 1000) {
-        // Check if at least 1 second has passed since the last click
-        updatePoints();
-        console.log("Function executed once per second on click.");
-        lastClickTime = now; // Update the last click timestamp
-      }
+    const now = Date.now(); // Get the current timestamp
+    if (now - lastClickTime >= 1000) {
+      // Check if at least 1 second has passed since the last click
+      setPoints();
+      lastClickTime = now; // Update the last click timestamp
     }
   });
+
+  setTime();
+  setPoints(false);
 });
 
 function startGame() {
-  gamePaused = false;
   timerInterval = setInterval(updateGame, 1000);
 }
 
 function pauseGame() {
-  gamePaused = true;
-  holes[newHole].removeChild(mole);
+  removeMole();
   clearInterval(timerInterval);
 }
 
@@ -45,12 +42,13 @@ function resetTimer() {
   clearInterval(timerInterval);
   seconds = 0;
   minutes = 0;
-  //Clear timer and points
+  //Clear timer, points and mole
   updateGame(false);
-  updatePoints(false);
+  setPoints(false);
 }
 
 function updateGame(updating = true) {
+  console.log("works");
   //Updating timer values
   if (updating) {
     seconds++;
@@ -60,24 +58,8 @@ function updateGame(updating = true) {
     }
     addMole();
   }
-  const timeString = `${String(minutes).padStart(2, "0")}:${String(
-    seconds
-  ).padStart(2, "0")}`;
-  document.getElementsByClassName("time")[0].textContent = timeString;
-}
 
-function addMole() {
-  newHole = Math.floor(Math.random() * 6);
-  while (newHole === lastHole) {
-    newHole = Math.floor(Math.random() * 6);
-  }
-  holes[newHole].appendChild(mole);
-  mole.classList.add("active-mole");
-  lastClickTime = 0;
-  setTimeout(() => {
-    mole.classList.remove("active-mole");
-  }, 1000);
-  lastHole = newHole;
+  setTime();
 }
 
 buttons.forEach((button) => {
@@ -102,7 +84,7 @@ buttons.forEach((button) => {
 // Move Mole
 
 // Points
-function updatePoints(updating = true) {
+function setPoints(updating = true) {
   if (updating) {
     let pointsValue = parseInt(points.innerText);
     pointsValue++;
@@ -111,4 +93,31 @@ function updatePoints(updating = true) {
   } else {
     points.innerHTML = "0";
   }
+}
+
+// Utility functions
+
+function addMole() {
+  newHole = Math.floor(Math.random() * 6);
+  while (newHole === lastHole) {
+    newHole = Math.floor(Math.random() * 6);
+  }
+  holes[newHole].appendChild(mole);
+  mole.classList.add("active-mole");
+  lastClickTime = 0;
+  setTimeout(() => {
+    mole.classList.remove("active-mole");
+  }, 1000);
+  lastHole = newHole;
+}
+
+function removeMole() {
+  holes[newHole].removeChild(mole);
+}
+
+function setTime() {
+  const timeString = `${String(minutes).padStart(2, "0")}:${String(
+    seconds
+  ).padStart(2, "0")}`;
+  document.getElementsByClassName("time")[0].textContent = timeString;
 }
